0a0b95a0fe1cb42f71097e33e97aeb80c2966435
Markus Rose <markus.m.rose@gmail.com>
2019-02-04 22:14:55 -0500

    Rewrote confocalsim.py for better usability. Way more userfriendly

--- confocalsim.py
+++ confocalsim.py
@@ -1,128 +1,172 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 from skimage import io
 
-
+#Gaussian Beam intensity
 def beamint(pos1,pos2,amp=1,w0=1):
-    return amp * np.exp(0 - (((pos1-pos2)**2).sum())/w0**2)
+    return amp * np.exp(0 - 2*(((pos1-pos2)**2).sum())/w0**2)
 
-def createMobileImage(numpixel=100,pixelsize=0.1,tau=0.001,w0=0.3,N=10,D=1e-9,subtau=10):
-    #all units in um and s    
-    i_map = np.zeros((numpixel,numpixel)) #pixelmap/ image
-    i_list = i_map.flatten()    #flattened pixelmap (list of intensities)
-
+def makeTracks(num_particles, diff_const, numpixel, pixelsize, tau, subtau):
     #track coordinates in um
     tracks = []
-    for i in range(N):
+    for i in range(num_particles):
         #format: t = [[xcoords],[ycoords]]
-        t = np.array([np.random.normal(loc=0.0,scale=np.sqrt(2*D*tau/subtau),size=(numpixel*numpixel*subtau)).cumsum(),
-                np.random.normal(loc=0.0,scale=np.sqrt(2*D*tau/subtau),size=(numpixel*numpixel*subtau)).cumsum()])
-        #print(t)
+        if diff_const > 0:
+            t = np.array([np.random.normal(loc=0.0,scale=np.sqrt(2*diff_const*tau/subtau),size=(numpixel*numpixel*subtau)).cumsum(),
+                np.random.normal(loc=0.0,scale=np.sqrt(2*diff_const*tau/subtau),size=(numpixel*numpixel*subtau)).cumsum()])
+        else:
+            t = np.zeros((2,numpixel*numpixel*subtau))
         startpos = numpixel*pixelsize*np.random.rand(2)
         #print(startpos)
         t[0] += startpos[0]
         t[1] += startpos[1]
         #print(t)
         tracks.append(t)
+    return tracks
 
-    for t in range(len(i_list)):
-        xcoord = (t % numpixel)
-        ycoord = (t-xcoord)/numpixel * pixelsize
-        xcoord *= pixelsize
-        
-        for track in tracks:
-            saver = 0
-            for j in range(subtau):
-                 saver += beamint(np.array([xcoord,ycoord]),track[:,t*subtau+j],w0=w0,amp=1/subtau)
-                 #saver /= subtau
-            i_list[t] += saver
-
-    i_map = i_list.reshape((numpixel,numpixel))
-    #print(i_map)
-    return i_map, tracks
-
-def createStationaryImage(numpixel=100,pixelsize=0.1,w0=0.3,N=10):
+def makeIntensityMap(tracks, numpixel, pixelsize, width):
+    #all units in um and s    
     i_map = np.zeros((numpixel,numpixel)) #pixelmap/ image
-
-    '''
-    def gaussian(pos,w,amp=1):
-        xcent = 5 + pos[0] - int(pos[0])
-        ycent = 5 + pos[0] - int(pos[0])
-        gmap = np.zeros((numpixel,numpixel))
-        for i in range(len(gmap)):
-            for j in range(len(gmap[i])):
-                gmap[i,j] = amp * np.exp(0 - (xcent-j-5)**2/w[1]**2+(ycent-i-5)**2/w[0]**2)
-        return gmap
-    '''
-
-    #track coordinates in um
-    tracks = []
-    for i in range(N):
-        #format: t = [[xcoords],[ycoords]]
-        startpos = numpixel*pixelsize*np.random.rand(2)
-        #print(startpos)
-        tracks.append(startpos)
-
     i_list = i_map.flatten()    #flattened pixelmap (list of intensities)
-    for t in range(len(i_list)):
-        xcoord = (t % numpixel)
-        ycoord = (t-xcoord)/numpixel * pixelsize
-        xcoord *= pixelsize
-        
-        for track in tracks:
-            i_list[t] += beamint(np.array([xcoord,ycoord]),track,w0=w0,amp=1)
+    
+    #subtau: subdivisions for pixel dwell time
+    if len(tracks) > 0:
+        subtau = int(len(tracks[0][0])/(numpixel*numpixel))
+        #make the intensity map
+        for t in range(len(i_list)):
+            xcoord = (t % numpixel)
+            ycoord = (t-xcoord)/numpixel * pixelsize
+            xcoord *= pixelsize
+            
+            for track in tracks:
+                if subtau >= 1:
+                    saver = 0
+                    for j in range(subtau):
+                         saver += beamint(np.array([xcoord,ycoord]),track[:,t*subtau+j],w0=width,amp=1/subtau)
+                    i_list[t] += saver
+                else:
+                    i_list[t] += beamint(np.array([xcoord,ycoord]),track[:,0],w0=width,amp=1)
 
     i_map = i_list.reshape((numpixel,numpixel))
-
-    return i_map, tracks
-
-def createNoiseImage(numpixel=100,noise=1.):
-    i_map = np.abs(np.random.normal(loc=0.1,scale=noise,size=[100,100]))
     return i_map
 
 
-def simulateConfocal(nmobile,nimmobile,fnoise,numpix=100,diffconst=1.0,width=0.3,noise=0.2,filename="confocalImage"):
-    numpixel = numpix
-    pixelsize = 0.1 #um
-    tau = 0.001 #s
-    w0 = width #um
-    Nmob = nmobile #number of mobile particles
-    Nstat = nimmobile #number of stationary particles
-    Nnoise = noise #noise amount
-    D = diffconst #um^2/s
-    subtau = 10
+#create a noise image
+def createNoiseImage(numpixel,stdNoise,avNoise=0):
+    if stdNoise > 0:
+        i_map = np.abs(np.random.normal(loc=avNoise,scale=stdNoise,size=[numpixel,numpixel]))
+    else:
+        i_map = np.zeros((numpixel,numpixel))+avNoise
+    return i_map
 
+
+def createFullImageFromTracks(tracks_mobile,tracks_immobile,std_noise,numpixel,pixelsize,w0,cpp):
+    #Make the image
+    #create mobile image map
+    i_map_mobile = makeIntensityMap(tracks_mobile,numpixel,pixelsize,w0)
+    #create immobile image map
+    i_map_immobile = makeIntensityMap(tracks_immobile,numpixel,pixelsize,w0)
+    #create background noise
+    i_map_noise = createNoiseImage(numpixel,std_noise)
+    #add images together
+    i_map = i_map_mobile + i_map_immobile + i_map_noise
+    #scale to CPP value
+    i_map = i_map * cpp
+    #Ensure positive values
+    i_map[i_map < 0] = 0
+    #Add poisson noise
+    i_map = np.random.poisson(i_map)
+    #Have maximum pixel value
+    i_map[i_map >= 2**16] = 2**16 - 1
+    #make a 16-bit integer array
+    i_map = i_map.astype(np.uint16)
+    return i_map
+
+#main function: create the full confocal image
+def simSingleChannelConfocal(nmobile, nimmobile, std_noise, diffconst, filename="confocalImage"):
+    pixelsize = 0.1 #um
+    numpixel=100
+    tau = 0.001 #s
+    w0 = 0.3 #um
+    subtau = 10
     cpp = 20 #kHz
 
-    #add mobile particles
-    i_map_mob, tracks = createMobileImage(numpixel,pixelsize,tau,w0,Nmob,D,subtau)
-    #add immobile particles
-    i_map_stat, parts = createStationaryImage(numpixel,pixelsize,w0,Nstat)
+    #Build the particles:
+    #create mobile tracks:
+    tracks_mobile = makeTracks(nmobile, diffconst ,numpixel, pixelsize, tau, subtau)
+    #create stationary particles:
+    tracks_immobile = makeTracks(nimmobile, 0, numpixel, pixelsize, tau, 1)
+    #Make the images from tracks
+    i_map = createFullImageFromTracks(tracks_mobile,tracks_immobile,std_noise,numpixel,pixelsize,w0,cpp)
+    #save the image and the track plot in a PNG
+    savePlot(i_map,tracks_mobile,tracks_immobile)
 
-    #add background noise
-    if fnoise:
-        i_map_noise = createNoiseImage(numpixel,Nnoise)
-    else:
-        i_map_noise = np.zeros((numpixel,numpixel))
+    return 
+
+def simBoundDualChannelConfocal(nmobile, nimmobile, std_noise, diffconst, offset=[0,0], filename="confocalImage"):
+    pixelsize = 0.1 #um
+    numpixel=100
+    tau = 0.001 #s
+    w0 = 0.3 #um
+    subtau = 10
+    cpp = 20 #kHz
+
+    #Build the particles:
+    #create mobile tracks:
+    ch0_tracks_mobile = makeTracks(nmobile, diffconst ,numpixel, pixelsize, tau, subtau)
+    #create stationary particles:
+    ch0_tracks_immobile = makeTracks(nimmobile, 0, numpixel, pixelsize, tau, 1)
+    #Make the images from tracks
+    ch0_i_map = createFullImageFromTracks(ch0_tracks_mobile,ch0_tracks_immobile,std_noise,numpixel,pixelsize,w0,cpp)
+    #save the image and the track plot in a PNG
+    savePlot(ch0_i_map,ch0_tracks_mobile,ch0_tracks_immobile,filename="CH0_Plot")
     
-    i_map = i_map_mob + i_map_stat + i_map_noise
+    #offset mobile and immobile tracks by "offset"
+    ch1_tracks_mobile = []
+    for track in ch0_tracks_mobile:
+        saver1 = track[0] + offset[0]*pixelsize
+        saver2 = track[1] + offset[1]*pixelsize
+        ch1_tracks_mobile.append(np.array([saver1,saver2]))
+    ch1_tracks_immobile = []
+    for track in ch0_tracks_immobile:
+        saver1 = track[0] + offset[0]*pixelsize
+        saver2 = track[1] + offset[1]*pixelsize
+        ch1_tracks_immobile.append(np.array([saver1,saver2]))
+    #Make the images from tracks
+    ch1_i_map = createFullImageFromTracks(ch1_tracks_mobile,ch1_tracks_immobile,std_noise,numpixel,pixelsize,w0,cpp)
+    #save the image and the track plot in a PNG
+    savePlot(ch1_i_map,ch1_tracks_mobile,ch1_tracks_immobile,filename="CH1_Plot")
 
-    i_map = i_map * cpp 
+    return [ch0_i_map, ch1_i_map]
 
-    i_map = np.random.poisson(i_map)
+def simFreeDualChannelConfocal(nmobile, nimmobile, std_noise, diffconst, filename="confocalImage"):
+    pixelsize = 0.1 #um
+    numpixel=100
+    tau = 0.001 #s
+    w0 = 0.3 #um
+    subtau = 10
+    cpp = 20 #kHz
 
-    i_map[i_map >= 2**16] = 2**16 - 1
+    i_maps = []
+    for i in range(2):
+        #Build the particles:
+        #create mobile tracks:
+        tracks_mobile = makeTracks(nmobile, diffconst ,numpixel, pixelsize, tau, subtau)
+        #create stationary particles:
+        tracks_immobile = makeTracks(nimmobile, 0, numpixel, pixelsize, tau, 1)
+        #Make the images from tracks
+        i_map = createFullImageFromTracks(tracks_mobile,tracks_immobile,std_noise,numpixel,pixelsize,w0,cpp)
+        #save the image and the track plot in a PNG
+        savePlot(i_map,tracks_mobile,tracks_immobile,filename="CH{:1d}_Plot".format(i))
+        i_maps.append(i_map)
+    return i_maps
 
-    i_map = i_map.astype(np.uint16)
-
-    return i_map,tracks,parts
-    
+#save the image
 def saveTIFF(i_map,filename):
     io.imsave(filename+".tif",i_map)
     return
-    
+
+#save the plot file
 def savePlot(i_map,tracks,parts,pixelsize=0.1,filename="exampleImage"):
     numpixel = len(i_map)
     fig1 = plt.figure(figsize=(12,7))
@@ -137,7 +181,7 @@ def savePlot(i_map,tracks,parts,pixelsize=0.1,filename="exampleImage"):
         ax2.plot(track[0,0],track[1,0],'go')
         ax2.plot(track[0,-1],track[1,-1],'ro')
     for part in parts:
-        ax2.plot(part[0],part[1],'ko')
+        ax2.plot(part[0,0],part[1,0],'ko')
     ax2.plot([-1,-2],[-1,-2],'y-',label="mobile tracks")
     ax2.plot([-1],[-1],'ko',label="immobile")
     ax2.plot([-1],[-1],'go',label="mobile start")
@@ -149,11 +193,12 @@ def savePlot(i_map,tracks,parts,pixelsize=0.1,filename="exampleImage"):
     ax2.set_ylabel(r"y [$\mu$m]")
     ax2.legend(bbox_to_anchor=(0,0,1.0,-0.13),loc=2,ncol=2,borderaxespad=0.,mode='expand')
     plt.savefig(filename+".png",dpi=70)
-    #plt.show()
+    plt.show()
     plt.close("all")
     return
 
 if __name__=="__main__":
-    imap,tracks,parts = simulateConfocal(nmobile=0,nimmobile=3,fnoise=False)
-    savePlot(imap,tracks,parts)
-
+    print("Testing the simulation")
+    #simSingleChannelConfocal(nmobile=10 ,nimmobile=10 ,std_noise=0.2,diffconst=2.0)
+    simBoundDualChannelConfocal(nmobile=10 ,nimmobile=10 ,std_noise=0.2,diffconst=2.0,offset=[0,0])
+    #simFreeDualChannelConfocal(nmobile=0 ,nimmobile=10 ,std_noise=0.2,diffconst=2.0)
